d24315c68533c3310edd7b55e7b13c6bfa3ca24e
hydewww <2208493266@qq.com>
2017-05-13 17:03:15 +0800

    加入乘客next指针

--- coding/passenger.h
+++ coding/passenger.h
@@ -5,9 +5,10 @@ typedef struct Passenger
 	int State;//服务状态 0未分配，1已分配窗口，2已开始安检，3已离开
 	int SerNum;//窗口
 	long TaskTime;//安检时间
+	int id;
+	Passenger *next;
 } Passenger;
-Passenger Pas[1000];//乘客数组
-#define MaxPass 1000  //定义最大乘客数量
+
 #endif
 
 
