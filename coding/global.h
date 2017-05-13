b1030d17159464f78119e0ae8ba6aff9e53d83c7
hydewww <2208493266@qq.com>
2017-05-13 17:02:52 +0800

    改了排版

--- coding/global.h
+++ coding/global.h
@@ -17,24 +17,28 @@
 extern int AirportState;//机场状态
 extern int TotalOdinCus;//当前总的乘客人数
 extern int OdinWatNum;//当前乘客等待人数
-extern int WinNum;//安检口工作数目
-extern int PassengerArriveTask;//给到达的乘客安排窗口号码
+
 extern clock_t TimeStart;//开始时间
 extern clock_t TimeFinish;//结束时间 
-extern clock_t TimeNow;//现在的时间
-extern int NumOfWin;//安检口总数目
-extern int MinRestSec;//安检口口最小休息时长
-extern int MaxRestSec;//安检口最大休息时长
-extern int MaxCustSingleLine;// 单队列最大等待乘客数
-extern int MaxLines;// 蛇形缓冲区最多由MaxLines个直队组成
-extern int MaxSeqLen;// 最大允许等待长度
-extern int EasySeqLen;// 短期等待长度
-extern int MinCheck;// 最少开放安检口数量
-extern int MaxCheck;// 最多开放安检口数量
-extern int MaxCustCheck;// 安检口队伍最大长度
-extern int MaxSec;// 安检口最大安检时长 单位是秒
-extern int MinSec;// 安检口最小安检时长 单位是秒
+extern clock_t TimeNow;	//现在的时间
+
+extern int MaxCustSingleLine;//单队列最大等待乘客数
+extern int MaxLines;		// 蛇形缓冲区最多由MaxLines个直队组成
+extern int MaxSeqLen;		// 最大允许等待长度
+extern int EasySeqLen;		// 短期等待长度
+
+extern int NumOfWin;	//安检口总数目
+extern int WinNum;		//安检口工作数目
+extern int MinRestSec;	//安检口口最小休息时长
+extern int MaxRestSec;	//安检口最大休息时长
+extern int MinCheck;	//最少开放安检口数量
+extern int MaxCheck;	//最多开放安检口数量
+extern int MaxCustCheck;//安检口队伍最大长度
+extern int MaxSec;		//安检口最大安检时长 单位是秒
+extern int MinSec;		//安检口最小安检时长 单位是秒
+extern int PassengerArriveTask;//给到达的乘客安排窗口号码
 
 //函数声明
 void MainPara();
+
 #endif
