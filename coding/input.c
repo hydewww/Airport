16430a3e92788ee3ec5174821f7f9f22c7e30a57
wxhhhhh <467613685@qq.com>
2017-05-13 19:32:15 +0800

    输出函数

---
+++ coding/input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "global.h"
+#include "passenger.h"
+#include "window.h"
+
+void MainInput();
+void AirportOnServe();
+void DistriNum(int);
+
+void MainInput() {
+
+	FILE *finput = fopen("input.dat", "rb+");
+
+
+}
