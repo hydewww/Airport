8b5874e5ec460f3369e66151a317831b74e817a6
wxhhhhh <467613685@qq.com>
2017-05-14 17:19:06 +0800

    头文件的更新

--- coding/global.h
+++ coding/global.h
@@ -17,39 +17,38 @@
 extern int AirportState;//机场状态
 extern int TotalOdinCus;//当前总的乘客人数
 extern int OdinWatNum;//当前乘客等待人数
-
-extern clock_t TimeStart;//开始时间
-extern clock_t TimeFinish;//结束时间 
-extern clock_t TimeNow;	//现在的时间
-
-extern int MaxCustSingleLine;//单队列最大等待乘客数
-extern int MaxLines;		// 蛇形缓冲区最多由MaxLines个直队组成
-extern int MaxSeqLen;		// 最大允许等待长度
-extern int EasySeqLen;		// 短期等待长度
-
-extern int NumOfWin;	//安检口总数目
-extern int WinNum;		//安检口工作数目
-extern int MinRestSec;	//安检口口最小休息时长
-extern int MaxRestSec;	//安检口最大休息时长
-extern int MinCheck;	//最少开放安检口数量
-extern int MaxCheck;	//最多开放安检口数量
-extern int MaxCustCheck;//安检口队伍最大长度
-extern int MaxSec;		//安检口最大安检时长 单位是秒
-extern int MinSec;		//安检口最小安检时长 单位是秒
+extern int WinNum;//安检口工作数目
 extern int PassengerArriveTask;//给到达的乘客安排窗口号码
+extern time_t TimeStart;//开始时间
+extern time_t TimeFinish;//结束时间 
+extern time_t TimeNow;//现在的时间
+extern int NumOfWin;//安检口总数目
+extern int MinRestSec;//安检口口最小休息时长
+extern int MaxRestSec;//安检口最大休息时长
+extern int MaxCustSingleLine;// 单队列最大等待乘客数
+extern int MaxLines;// 蛇形缓冲区最多由MaxLines个直队组成
+extern int MaxSeqLen;// 最大允许等待长度
+extern int EasySeqLen;// 短期等待长度
+extern int MinCheck;// 最少开放安检口数量
+extern int MaxCheck;// 最多开放安检口数量
+extern int MaxCustCheck;// 安检口队伍最大长度
+extern int MaxSec;// 安检口最大安检时长 单位是秒
+extern int MinSec;// 安检口最小安检时长 单位是秒
 
-struct entry {
+typedef struct entry {
 	int no; //事件序号
 	int sec;//事件发生时间间隔
 	char type;//事件类型：C-乘客到达，X-安检口申请暂停，Q-下班；
 	int mans; //事件属性1-到达人数
-	int check; //事件属性2-申请暂停的安检口编号 
-	int ev_valid; //判断事件是否读取
-};
+	int check; //事件属性2-申请休息的安检口编号 
+	int ev_valid; //事件是否被读取
+}entry;
+
 
 //函数声明
-void MainInput();
-void AirportOnServe();
+int MainPara();//维护配置文件
+void DistriNum(entry event);//为乘客分配号码并插入排队缓冲区
+void EventOutput(int EventType, int PasID, int WinID);
 void DistriNum(int);
 void WinRun();
 long SerTaskTime();
@@ -57,6 +56,4 @@ int  MainPara();
 void CusOutput();
 void NowTime();
 void RegularOutput();
-void ResultOutput();
-
 #endif
--- coding/passenger.h
+++ coding/passenger.h
@@ -3,11 +3,14 @@
 typedef struct Passenger
 {
 	int State;//服务状态 0未分配，1已分配窗口，2已开始安检，3已离开
+	int id;//乘客id
 	int SerNum;//窗口
 	long TaskTime;//安检时间
-	int id;
-	Passenger *next;
+	 Passenger *next;
 } Passenger;
+
+Passenger* Queuehead;//排队队列头指针，next指向第一位乘客
+Passenger* Queuetail;//排队队列尾指针，始终指向最后一位乘客
 
 #endif
 
