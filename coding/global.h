c6f45fbc57f3c9c85fecf2f32cebc02ce2d605c8
hydewww <2208493266@qq.com>
2017-05-15 17:31:42 +0800

    函数名改动 叒改了排版

--- coding/global.h
+++ coding/global.h
@@ -5,7 +5,7 @@
 //机场状态
 #define OffWork 0    //下班
 #define OnWork 1     //正在工作
-#define ShutDown 2 
+#define ShutDown 2	 //正在下班
 //窗口状态
 #define CloseWin 0   //窗口关闭
 #define OpenWin 1    //窗口开启
@@ -18,23 +18,27 @@
 extern int AirportState;//机场状态
 extern int TotalOdinCus;//当前总的乘客人数
 extern int OdinWatNum;//当前乘客等待人数
-extern int WinNum;//安检口工作数目
 extern int PassengerArriveTask;//给到达的乘客安排窗口号码
-extern time_t TimeStart;//开始时间
-extern time_t TimeFinish;//结束时间 
-extern time_t TimeNow;//现在的时间
-extern int NumOfWin;//安检口总数目
-extern int MinRestSec;//安检口口最小休息时长
-extern int MaxRestSec;//安检口最大休息时长
+extern int new_sigh;//同步锁
+//排队缓冲区
 extern int MaxCustSingleLine;// 单队列最大等待乘客数
 extern int MaxLines;// 蛇形缓冲区最多由MaxLines个直队组成
 extern int MaxSeqLen;// 最大允许等待长度
 extern int EasySeqLen;// 短期等待长度
+//安检口
+extern int NumOfWin;//安检口总数目
+extern int WinNum;//安检口工作数目
 extern int MinCheck;// 最少开放安检口数量
 extern int MaxCheck;// 最多开放安检口数量
 extern int MaxCustCheck;// 安检口队伍最大长度
 extern int MaxSec;// 安检口最大安检时长 单位是秒
 extern int MinSec;// 安检口最小安检时长 单位是秒
+extern int MinRestSec;//安检口口最小休息时长
+extern int MaxRestSec;//安检口最大休息时长
+//时间
+extern time_t TimeStart;//开始时间
+extern time_t TimeFinish;//结束时间 
+extern time_t TimeNow;//现在的时间
 
 typedef struct entry {
 	int no; //事件序号
@@ -53,8 +57,10 @@ int MainPara();//维护配置文件
 double random();
 void MainInput();
 void AirportOnServe();
+//Cmdinput.c
+unsigned __stdcall CmdInput(void* pArguments);
 //state.cpp
-void DistriNum(entry event);//为乘客分配号码并插入排队缓冲区
+void DistriNum(entry *event);//为乘客分配号码并插入排队缓冲区
 void WinRun();
 void PreWinRun();
 int CheckWin();//判断安检口能否关闭
@@ -64,5 +70,4 @@ void StateTrans(entry * event);//总控制函数
 void StatusOutputCmd();
 void EventOutputFile(char event, int id, int no);
 void StatusOutputFile();
-//void NowTime();
 #endif
