7d4616bf6c9ecf76cd91fedc478632e1aff6ef09
hydewww <2208493266@qq.com>
2017-05-23 16:24:23 +0800

    改了下输出

--- coding/output.c
+++ coding/output.c
@@ -6,11 +6,43 @@
 #include<Windows.h>
 #include<process.h>
 
+void WinPrint(Window* win) {
+	switch (win->WinState) {
+	case 0:printf("    关闭"); break;
+	case 1:printf("  空闲中"); break;
+	case 2:printf("  服务中"); break;
+	case 3:printf("  休息中"); break;
+	case 4:printf("准备休息"); break;
+	case 5:printf("准备关闭"); break;
+	default:printf("\n窗口状态异常！\n"); exit(1); break;
+	}
+	//正在安检
+	if (win->NowPas) {
+		printf("\t正在安检: %3d ", win->NowPas->id);
+	}
+	else {
+		printf("\t正在安检:  无 ");
+	}
+	//窗口队列
+	if (win->WinHead != win->WinTail) {	//判空
+		printf(" , 队列:");
+		Passenger* cur = win->WinHead;
+		do {
+			cur = cur->next;
+			printf(" %3d", cur->id);
+		} while (cur != win->WinTail);
+	}
+
+}
+
+
 extern int ii;
 time_t PreFile;
 void StatusOutputFile() {
 	time(&TimeNow);
-	if ((difftime(TimeNow, PreFile)) <3)
+	double wtf;
+	wtf = difftime(TimeNow, PreFile);
+	if (wtf <3)
 		return;
 	PreFile = TimeNow;
 	FILE* fp;
@@ -18,8 +50,7 @@ void StatusOutputFile() {
 		puts("Error: cannot open the file \"output.txt\".");
 		exit(1);
 	}
-	fprintf(fp,"--------------------------------------\n");
-	fprintf(fp, "现在正在发生第%d个事件\n", ii);
+	fprintf(fp, "-----------------------------------------------\n");
 	fprintf(fp, "时间 : %s", ctime(&TimeNow));
 	//机场状态
 	switch (AirportState) {
@@ -28,44 +59,47 @@ void StatusOutputFile() {
 	case 2:fprintf(fp, "工作状态 = 准备下班\n"); break;
 	default: fprintf(fp, "机场状态异常"); exit(1);
 	}
-	//排队缓冲区状态
-	if (Queuehead->next != NULL)
-		fprintf(fp, "排队缓冲区总人数: %d ,首乘客: %d , 尾乘客: %d\n", OdinLineWaitNum, Queuehead->next->id, Queuetail->id);		//排队缓冲区队首乘客编号，队尾乘客编号
-	else
-		fprintf(fp,"排队缓冲区总人数: 0\n");
+
 	//窗口状态
 	for (int i = 0; i < NumOfWin; i++) {
 		//窗口n
-		fprintf(fp, "WIN%d  ", i);
+		fprintf(fp,"WIN%d  ", i + 1);
 		//窗口状态
 		switch (Win[i].WinState) {
-		case 0:fprintf(fp, "    关闭"); break;
-		case 1:fprintf(fp, "  空闲中"); break;
-		case 2:fprintf(fp, "  服务中"); break;
-		case 3:fprintf(fp, "  休息中"); break;
-		case 4:fprintf(fp, "准备休息"); break;
-		case 5:fprintf(fp, "准备关闭"); break;
-		default:fprintf(fp, "\n窗口状态异常！\n"); exit(1); break;
+		case 0:fprintf(fp,"    关闭"); break;
+		case 1:fprintf(fp,"  空闲中"); break;
+		case 2:fprintf(fp,"  服务中"); break;
+		case 3:fprintf(fp,"  休息中"); break;
+		case 4:fprintf(fp,"准备休息"); break;
+		case 5:fprintf(fp,"准备关闭"); break;
+		default:fprintf(fp,"\n窗口状态异常！\n"); exit(1); break;
 		}
 		//正在安检
 		if (Win[i].NowPas) {
-			fprintf(fp, "\t正在安检: %3d ", Win[i].NowPas->id);
+			fprintf(fp,"\t正在安检: %3d ", Win[i].NowPas->id);
 		}
 		else {
-			fprintf(fp, "\t正在安检:  无 ");
+			fprintf(fp,"\t正在安检:  无 ");
 		}
 		//窗口队列
 		if (Win[i].WinHead != Win[i].WinTail) {	//判空
-			fprintf(fp, " , 队列:");
+			fprintf(fp," , 队列:");
 			Passenger* cur = Win[i].WinHead;
 			do {
 				cur = cur->next;
-				fprintf(fp, " %3d", cur->id);
+				fprintf(fp," %3d", cur->id);
 			} while (cur != Win[i].WinTail);
 		}
-		fprintf(fp, "\n");
+		fprintf(fp,"\n");
 	}
-	fprintf(fp, "--------------------------------------");
+
+	//排队缓冲区状态
+	if (Queuehead->next != NULL) 
+		fprintf(fp,"\n\t\t排队缓冲区\n总人数: %d ,首乘客: %d , 尾乘客: %d , 队列数: %d\n", OdinLineWaitNum, Queuehead->next->id, Queuetail->id, (OdinLineWaitNum + MaxCustSingleLine - 1) / MaxCustSingleLine);		//排队缓冲区队首乘客编号，队尾乘客编号
+	else
+		fprintf(fp,"\n排队缓冲区总人数: 0\n");
+
+	fprintf(fp, "-----------------------------------------------\n");
 }
 
 void EventOutputFile(char event, int PasID, int WinID) {	//PasID乘客 WinID安检口
@@ -103,7 +137,8 @@ void StatusOutputCmd() {
 	if (((difftime(TimeNow, PreCmd)) < 1) && (AirportState != OffWork))
 		return;
 	PreCmd = TimeNow;	//记录时间
-	puts("--------------------------------------");
+	//puts("--------------------------------------");
+	system("CLS");
 	printf("现在正在发生第%d个事件\n", ii);//
 	printf("时间 : %s", ctime(&TimeNow));
 	//机场状态
@@ -116,33 +151,34 @@ void StatusOutputCmd() {
 	//窗口状态
 	for (int i = 0; i < NumOfWin; i++) {
 		//窗口n
-		printf("WIN%d  ", i);
-		//窗口状态
-		switch (Win[i].WinState) {
-		case 0:printf("    关闭"); break;
-		case 1:printf("  空闲中"); break;
-		case 2:printf("  服务中"); break;
-		case 3:printf("  休息中"); break;
-		case 4:printf("准备休息"); break;
-		case 5:printf("准备关闭"); break;
-		default:printf("\n窗口状态异常！\n"); exit(1); break;
-		}
-		//正在安检
-		if (Win[i].NowPas) {
-			printf("\t正在安检: %3d ", Win[i].NowPas->id);
-		}
-		else {
-			printf("\t正在安检:  无 ");
-		}
-		//窗口队列
-		if (Win[i].WinHead != Win[i].WinTail) {	//判空
-			printf(" , 队列:");
-			Passenger* cur = Win[i].WinHead;
-			do {
-				cur = cur->next;
-				printf(" %3d", cur->id);
-			} while (cur != Win[i].WinTail);
-		}
+		printf("WIN%d  ", i+1);
+		////窗口状态
+		//switch (Win[i].WinState) {
+		//case 0:printf("    关闭"); break;
+		//case 1:printf("  空闲中"); break;
+		//case 2:printf("  服务中"); break;
+		//case 3:printf("  休息中"); break;
+		//case 4:printf("准备休息"); break;
+		//case 5:printf("准备关闭"); break;
+		//default:printf("\n窗口状态异常！\n"); exit(1); break;
+		//}
+		////正在安检
+		//if (Win[i].NowPas) {
+		//	printf("\t正在安检: %3d ", Win[i].NowPas->id);
+		//}
+		//else {
+		//	printf("\t正在安检:  无 ");
+		//}
+		////窗口队列
+		//if (Win[i].WinHead != Win[i].WinTail) {	//判空
+		//	printf(" , 队列:");
+		//	Passenger* cur = Win[i].WinHead;
+		//	do {
+		//		cur = cur->next;
+		//		printf(" %3d", cur->id);
+		//	} while (cur != Win[i].WinTail);
+		//}
+		WinPrint(&Win[i]);
 		printf("\n");
 	}
 
@@ -168,6 +204,6 @@ void StatusOutputCmd() {
 		printf("\n");
 	}//if
 	else
-		puts("排队缓冲区总人数: 0");
+		puts("\n排队缓冲区总人数: 0");
 	//puts("--------------------------------------");
 }
