2dba115278f3eca38a52ec345af703df7c1d31c1
hydewww <2208493266@qq.com>
2017-05-27 12:16:48 +0800

    适配queue 新增释放内存

--- coding/main.c
+++ coding/main.c
@@ -8,6 +8,7 @@
 #include "global.h"
 #include "passenger.h"
 #include "window.h"
+#include "queue.h"
 
 Window* Win;
 Window* VIPWin;
@@ -16,8 +17,12 @@ time_t	TimeStart;
 time_t	TimeNow;
 time_t	TimeFinish;
 int		AirportState;
-int		PassengerArriveTask;	 /*没用过*/
 int lock = 0;
+Queue* OdinQueue;
+Queue* VipQueue;
+
+Queue* QueueEstablish();//创建队列
+void FreeMem();	//释放malloc内存
 
 void InitWin(Window* win) //窗口初始化
 {
@@ -29,6 +34,7 @@ void InitWin(Window* win) //窗口初始化
 	win->WinTail = win->WinHead;
 	win->NowPas = NULL;
 }
+
 
 void SetAndBegin() 
 {
@@ -57,17 +63,20 @@ void SetAndBegin()
 
 	fclose(fopen("output.txt", "w"));//清空output.txt
 	MainInput();//生成事件
-	time(&TimeStart);
+	OdinQueue = QueueEstablish();
+	VipQueue = QueueEstablish();
+
 	AirportState = OnWork;
-	QueueEstablish();
+	time(&TimeStart);
 }
 
 int main() {
 	lock = 0;
-	SetAndBegin();
 	mciSendString(TEXT("open 警报2.mp3 alias music"), NULL, 0, NULL);
 	HANDLE KeyBoard;
 	KeyBoard = (HANDLE)_beginthreadex(NULL, 0, KeyEvent, NULL, 0, NULL);
+
+	SetAndBegin();
 	while (AirportState!=OffWork)
 	{
 		lock = 1;
@@ -83,7 +92,37 @@ int main() {
 	}
 	WaitForSingleObject(KeyBoard, INFINITE);
 	CloseHandle(KeyBoard);
+	FreeMem();//释放malloc内存
 	system("pause");
 	return 0;
 }
 
+Queue* QueueEstablish()//创建队列头指针函数
+{
+	Queue* queue;
+	if (!(queue = (Queue*)malloc(sizeof(Queue))) || !(queue->QueueHead = (Passenger*)malloc(sizeof(Passenger)))) {
+		puts("申请缓冲区内存失败!");
+		exit(1);
+	}
+	queue->QueueHead->next = NULL;//开始无乘客
+	queue->QueueTail = queue->QueueHead;//尾指针和头指针同指
+	queue->SumNum = 0;
+	queue->WaitNum = 0;
+	return queue;
+}
+
+void FreeMem() {
+	for (int i = 0; i < NumOfWin; i++) {
+		free(Win[i].WinHead);
+	}
+	free(Win);
+	for (int i = 0; i < NumOfVIPWin; i++) {
+		free(VIPWin[i].WinHead);
+	}
+	free(VIPWin);
+	
+	free(OdinQueue->QueueHead);
+	free(OdinQueue);
+	free(VipQueue->QueueHead);
+	free(VipQueue);
+}
