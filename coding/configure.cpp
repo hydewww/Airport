d6b5109e1f088755142ebb1529f06fad49e4df0d
wxhhhhh <467613685@qq.com>
2017-06-18 11:30:15 +0800

    Revert "Revert "配置文件无法修改bug""
    This reverts commit 11f8d600f302fedf5722a0efbe4c3a58d6fa7361.

--- coding/configure.cpp
+++ coding/configure.cpp
@@ -225,11 +225,13 @@ void MainPara() {
 					fwrite(&EasySeqLen, sizeof(int), 1, fp);
 				}
 				if (judgeButton(msg.x, msg.y, 250, 600, 100, 50)) {
+					fclose(fp);
 					closegraph();
 					InitInter();
 					InitInterCheck();
 				}
 				if (judgeButton(msg.x, msg.y, 790, 600, 100, 50)) {
+					fclose(fp);
 					TCHAR OriginalStr[10];
 					InputBox(OriginalStr, 10, _T("请输入事件个数"));
 					EventNum = _ttoi(OriginalStr);
@@ -239,5 +241,4 @@ void MainPara() {
 			}
 		}
 	}
-	fclose(fp);
 }
